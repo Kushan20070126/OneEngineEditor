804b4fc8139238726644ac5bf53d24eeed5ba516
kushan <k14922160@gmail.com>
2026-09-01 02:22:33 -1100

    add correct App class / PeekMessage for test

--- OneEngineEditor/App.cpp
+++ OneEngineEditor/App.cpp
@@ -7,24 +7,17 @@ App::App()
 
 int App::Go()
 {
-	MSG msg;
-	BOOL gResult;
 
-	while ((gResult = GetMessage(&msg,nullptr,0,0)) > 0)
+
+	while (true)
 	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		if (const auto ecode = Window::ProcessMessages())
+		{
 
+			return *ecode;
+		}
 		DoFrame();
 	}
-
-	if (gResult == -1) {
-
-		throw CHWND_LAST_EXCEPT();
-
-	}
-
-	return msg.wParam;
 
 }
 
